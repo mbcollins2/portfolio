d26cdcbda8ae9f0b34a85d123d705fe16e3ee546
Matt Collins <mbcollins2@gmail.com>
2024-10-13 10:29:59 -0400

    Set up favicon

--- src/portfolio/__init__.py
+++ src/portfolio/__init__.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template 
+from flask import Flask, render_template
 from flask_caching import Cache
 
 app = Flask(__name__)
-cache = Cache(app, config={'CACHE_TYPE': 'SimpleCache'})
+cache = Cache(app, config={"CACHE_TYPE": "SimpleCache"})
 
 
 @app.route("/")
@@ -16,10 +16,12 @@ def home():
 def projects():
     return render_template("pages/projects.html")
 
+
 @app.route("/projects/portfolio-website")
 # @cache.cached(timeout=60)
 def project_page():
     return render_template("pages/projects/portfolio_website.html")
+
 
 # TODO - set up slugs for project page templates
 # @app.route("/projects/<slug>")
@@ -29,4 +31,4 @@ def project_page():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
