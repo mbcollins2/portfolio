7de3c36ebab803a2dc0819f932b35e4d66c56474
Matt Collins <mbcollins2@gmail.com>
2024-10-21 22:31:39 -0400

    Setting up markdown files

--- src/portfolio/__init__.py
+++ src/portfolio/__init__.py
@@ -1,14 +1,51 @@
+import os
+import json
 import tomllib
-from flask import Flask, render_template
+import markdown
+import frontmatter
+from datetime import datetime
+from markupsafe import Markup
 from flask_caching import Cache
+from flask import Flask, render_template
 
 # parse toml config file
 with open("pyproject.toml", "rb") as f:
     config = tomllib.load(f)
 
 
+PROJECTS_DIR = "src/portfolio/templates/pages/projects"
+
+
 app = Flask(__name__)
-cache = Cache(app, config={"CACHE_TYPE": "SimpleCache"})
+# cache = Cache(app, config={"CACHE_TYPE": "SimpleCache"})
+
+
+# TODO 
+# load all projects in project directory
+# parse all project metadata into a json with a name for the slug and then the metadata and content
+# order descending by date
+
+project_data = dict()
+for project_filename in os.listdir(PROJECTS_DIR):
+    if project_filename.endswith(".md"):
+        project_slug = project_filename[:-3].replace("_","-")
+
+        project_data[project_slug] = dict()
+
+        with open(os.path.join(PROJECTS_DIR, project_filename), "r") as f:
+            project_file = frontmatter.load(f)
+
+        project_data[project_slug]["project_metadata"] = project_file.metadata
+        project_data[project_slug]["project_content"] = markdown.markdown(project_file.content)
+
+# print("project_data", project_data)
+
+project_list = [{"title": x, "date": project_data[x]['project_metadata']['publish_month_year']} for x in project_data]
+sorted_project_list = [x['title'] for x in sorted(project_list, key=lambda x: datetime.strptime(x['date'], '%B %Y'), reverse=True)]
+
+# print("list", project_list)
+# print("list", [x['title'] for x in sorted(project_list, key=lambda x: datetime.strptime(x['date'], '%B %Y'), reverse=True)])
+
 
 
 @app.context_processor
@@ -27,20 +64,32 @@ def home():
 @app.route("/projects")
 # @cache.cached(timeout=60)
 def projects():
+
+    # TODO - get all of the projects in p
+
     return render_template("pages/projects.html")
 
 
-@app.route("/projects/portfolio-website")
+@app.route("/projects/<slug>")
 # @cache.cached(timeout=60)
-def project_page():
-    return render_template("pages/projects/portfolio_website.html")
+def project_page(slug):
 
+    # # load project markdown
+    # project_markdown_path = os.path.join(PROJECTS_DIR, f'{slug}.md').replace("-","_")
 
-# TODO - set up slugs for project page templates
-# @app.route("/projects/<slug>")
-# # @cache.cached(timeout=60)
-# def project_page(slug):
-#     return render_template("pages/projects/portfolio_website.html", slug=slug)
+    # with open(project_markdown_path, "r") as f:
+    #     project_file = frontmatter.load(f)
+
+    # project_metadata = project_file.metadata
+    # project_content = markdown.markdown(project_file.content)
+
+    return render_template(
+        "pages/project_page.html", 
+        # project_content=Markup(project_content),
+        # project_metadata=project_metadata
+        project_content=Markup(project_data[slug]["project_content"]),
+        project_metadata=project_data[slug]["project_metadata"]
+    )
 
 
 if __name__ == "__main__":
---
+++ src/portfolio/templates/pages/projects/bigquery_ml_tableau.md
@@ -0,0 +1,18 @@
+---
+"title": "Integrating BigQuery ML & Tableau"
+"status": "Active"
+"category": "Data Science"
+"publish_month_year": "October 2018"
+"project_summary": "Short summary text here"
+"technologies": [
+    "Python",
+    "Google Cloud",
+    "BigQuery",
+    "Tableau"
+]
+"preview_image": "url to image here"
+---
+
+
+### Overview
+Coming soon
